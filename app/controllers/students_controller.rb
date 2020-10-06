913ee0d2be132d19be5b478875801bea04317db1
rtetelbaum <rtetelbaum@mailbox.org>
2020-10-06 15:50:12 -0400

    lab done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,22 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :activate]
   
   def index
     @students = Student.all
   end
 
-  def show
-  end
+	def show
+		@student
+	end
+	
+	def activate
+		#@student = Student.find(params[:id])
+		@student.toggle(:active)
+		@student.save
+		#byebug
+		redirect_to student_path(@student)
+		# redirect_to "/students/:id"
+	end
 
   private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Roman", last_name: "Tetelbaum")
+Student.create(first_name: "John", last_name: "Smith")
+Student.create(first_name: "Tilda", last_name: "Swinton")
